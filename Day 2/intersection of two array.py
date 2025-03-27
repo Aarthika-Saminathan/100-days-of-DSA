79252abe88eb8b653f141b32033bed78fe23edf7
aarthika51 <aarthikasaminathan7896@gmail.com>
2025-03-27 23:31:26 +0530

    day 2

--- Day 2/intersection of two array.py
+++ Day 2/intersection of two array.py
@@ -37,3 +37,14 @@ def intersection_of_array(arr1,arr2):
 arr1=[1,2,3,4]
 arr2=[3,4,5,6]
 print(intersection_of_array(arr1,arr2))
+
+
+#method 3
+
+def intersection_of_array(arr1,arr2):
+    return list(set(arr1) & set(arr2))
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [2, 3, 5, 6]
+
+print(intersection_of_array(arr1,arr2))
